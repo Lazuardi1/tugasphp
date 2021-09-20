385020dfabcc62c355ce3306e751af2dd62898df
Lazuardi1 <87347424+Lazuardi1@users.noreply.github.com>
2021-09-20 09:39:54 +0700

    tugass

---
+++ lat01.php
@@ -0,0 +1,52 @@
+<?php 
+
+        $tulisan = 'saya belajar php';
+
+        echo $tulisan. '<br>';
+
+        $angka = 2021;
+
+        echo 'pada tahun ' .$angka. '<br>'; 
+
+    
+        var_dump($tulisan);
+
+        echo '<br>';
+
+        var_dump($angka);
+
+
+        $tulisan = 'saya belajar php';
+
+        echo $tulisan. '<br>';
+
+        $angka = '20,12';
+
+        echo 'pada tahun ' .$angka. '<br>'; 
+
+    
+        var_dump($tulisan);
+
+        echo '<br>';
+
+        var_dump($angka);
+
+
+
+        $tulisan = 'saya belajar php';
+
+        echo $tulisan. '<br>';
+
+        $angka = 2021;
+
+        echo 'pada tahun ' .$angka. '<br>'; 
+
+        /*
+        var_dump($tulisan);
+
+        echo '<br>';
+
+        var_dump($angka);
+        */
+        
+?>
